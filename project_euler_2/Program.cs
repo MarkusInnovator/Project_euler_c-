d401bfed0a3eb921835618d844fd2f780ba892c0
w3schools-test <test@w3schools.com>
2024-04-01 16:06:02 +0200

    project_euler_2/project_euler_2 created

---
+++ project_euler_2/Program.cs
@@ -0,0 +1,21 @@
+﻿static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+
+int limit = 4000000;
+int sumEven = 0;
+int n = 0;
+
+ while (Fibonacci(n) <= limit)
+        {
+            if (Fibonacci(n) % 2 == 0)
+                sumEven += Fibonacci(n);
+            n++;
+        }
+
+Console.WriteLine("Die Summe der geraden Zahlen in der Fibonacci-Folge unter " + limit + " ist: " + sumEven);

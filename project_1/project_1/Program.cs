1d5376e11173d77bd96460c3d0573c4185b9984c
w3schools-test <test@w3schools.com>
2024-04-01 02:09:04 +0200

    first task in c#

---
+++ project_1/project_1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int count = 0;
+for (int i = 0; i < 1000; i++)
+{
+   if (i % 3 == 0 || i % 5 == 0)
+   {
+      count += i;
+   } 
+}
+Console.WriteLine(count);
